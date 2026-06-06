4138ad4755f36b260bb1d1d369134cd1fe5b8289
khurramabbasoffical <ka1800441@gmail.com>
2026-06-06 23:54:55 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-const w=['Learn Quran Online','Learn Tajweed Online','Learn Islamic Studies Online'];let i=0;setInterval(()=>{document.getElementById('typing').textContent=w[i%w.length];i++;},1500);
+const words = [
+"Learn Quran Online",
+"Learn Tajweed Online",
+"Learn Islamic Studies Online",
+"Learn Quran Translation"
+];
+
+let i = 0;
+const typingElement = document.getElementById("typing");
+
+function changeText(){
+typingElement.style.opacity = 0;
+
+setTimeout(() => {
+typingElement.textContent = words[i];
+typingElement.style.opacity = 1;
+
+i++;
+if(i >= words.length){
+i = 0;
+}
+}, 300);
+}
+
+changeText();
+setInterval(changeText, 2000);
